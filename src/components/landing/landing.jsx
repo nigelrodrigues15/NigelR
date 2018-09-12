b5e300eb0afaa3d133d46e8301745166ab08f2d2
Nigel Rodrigues <nigel1@ualberta.ca>
2018-09-12 09:14:08 -0700

    gradient div

--- src/components/landing/landing.jsx
+++ src/components/landing/landing.jsx
@@ -11,6 +11,7 @@ class Landing extends React.Component {
   render () {
 
     return <div className="landing">
+        <div className="landing-backcolor-gradient"></div>
         <div className="landing-background">
           {/* <img src="./images/BrassGuitar.jpg" alt="" /> */}
         </div>
