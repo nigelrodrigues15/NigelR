f84532db2abec909c37b5053ae1a34cb617539fa
Nigel Rodrigues <nigel1@ualberta.ca>
2018-12-02 01:04:14 -0800

    photo

---
+++ src/components/photos/photos_container.js
@@ -0,0 +1,19 @@
+import { connect } from "react-redux";
+import Photos from "./photos";
+
+const mapStateToProps = (state, ownprops) => {
+    return {
+
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+
+    };
+};
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Photos);
