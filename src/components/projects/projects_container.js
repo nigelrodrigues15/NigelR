c68c61a2736312ace459aba9bc32f833f8174f79
Nigel Rodrigues <nigel1@ualberta.ca>
2018-12-02 01:04:35 -0800

    project

---
+++ src/components/projects/projects_container.js
@@ -0,0 +1,19 @@
+import { connect } from "react-redux";
+import Projects from "./projects";
+
+const mapStateToProps = (state, ownprops) => {
+    return {
+
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+
+    };
+};
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Projects);
