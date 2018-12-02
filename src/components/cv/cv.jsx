82a601746ff9d47d1e20593267a3ee08ef70f43f
Nigel Rodrigues <nigel1@ualberta.ca>
2018-12-02 08:10:11 -0800

    education

--- src/components/cv/cv.jsx
+++ src/components/cv/cv.jsx
@@ -18,6 +18,18 @@ class CV extends React.Component {
   render() {
     return <div className="cv">
         <h1>This is my CV</h1>
+        <div>
+          <h1>Education</h1>
+          <p>
+            • MEng, Mechanical and Mechatronics Engineering, Graduate
+            Diploma in Design Co-op, Ongoing • BSc, Mechanical Engineering
+            Co-op, Minor in Mathematics, 2018 • Louise McKinney Scholarship
+            • (top 1.5-2% in Mech. Eng. • NSERC USRA Research Award • Dean’s
+            Research Award • U of A Academic Excellence Capstone: Analyzed
+            failure modes of a disabled ski outrigger and redesigned hinge
+            to prevent stranded skiers.
+          </p>
+        </div>
       </div>;
   }
 }
