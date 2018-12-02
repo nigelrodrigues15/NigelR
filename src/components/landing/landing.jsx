8956dbd9abd9c3cb2600063a3627510e4dac4521
Nigel Rodrigues <nigel1@ualberta.ca>
2018-12-02 01:31:24 -0800

    query string

--- src/components/landing/landing.jsx
+++ src/components/landing/landing.jsx
@@ -1,30 +1,40 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 
-const landing = () => {
-  return <div className="landing">
-      <div className="greeting">Hello,</div>
-      <br />
-      <div className="name">Nigel Rodrigues</div>
-      <br />
-      <div className="options">
-        <Link to="/cv">
-          <div className="cv">CV</div>
-        </Link>
-        <Link to="/photography">
-          <div className="photography">Photography</div>
-        </Link>
-        <Link to="/projects" >
-          <div className="projects">Projects</div>
-        </Link>
-        <Link to="/aboutme" >
-          <div className="about">About</div>
-        </Link>
-        <Link to="/travel" >
-          <div className="travel">Travel</div>
-        </Link >
+class Landing extends React.Component {
+
+  constructor(props) {
+    super(props);
+  };
+
+  render () {
+
+    return(
+      <div className="landing">
+        <div className="greeting">Hello,</div>
+        <h1>{this.props.match.params.name ? this.props.match.params.name : null}</h1>
+        <br />
+        <div className="name">Nigel Rodrigues</div>
+        <br />
+        <div className="options">
+          <Link to="/cv">
+            <div className="cv">CV</div>
+          </Link>
+          <Link to="/photography">
+            <div className="photography">Photography</div>
+          </Link>
+          <Link to="/projects" >
+            <div className="projects">Projects</div>
+          </Link>
+          <Link to="/aboutme" >
+            <div className="about">About</div>
+          </Link>
+          <Link to="/travel" >
+            <div className="travel">Travel</div>
+          </Link >
+        </div>
       </div>
-    </div>;
-};
-
-export default landing;
+    );
+  };
+}
+export default withRouter(Landing);
