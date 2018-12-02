0f26a5826011d4df82c11b731a7e5dbdbb43eb20
Nigel Rodrigues <nigel1@ualberta.ca>
2018-12-02 00:36:43 -0800

    projects

---
+++ src/components/photos/cv_container.js
@@ -0,0 +1,19 @@
+import { connect } from "react-redux";
+import CV from "./cv";
+
+const mapStateToProps = (state, ownprops) => {
+    return {
+
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+
+    };
+};
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(CV);
