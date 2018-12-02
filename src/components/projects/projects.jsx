6b746759835fc0759f864af1a1adf3f831478b6c
Nigel Rodrigues <nigel1@ualberta.ca>
2018-12-02 01:04:30 -0800

    projects

---
+++ src/components/projects/projects.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link, withRouter } from "react-router-dom";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+
+class Projects extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentWillReceiveProps(nextProps) {}
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <div className="cv">
+        This is my CV
+      </div>
+    );
+  }
+}
+
+export default withRouter(Projects);
