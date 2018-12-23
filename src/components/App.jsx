475eb7277ea2c76fe77fd72581908e6734ce4439
Nigel Rodrigues <nigel1@ualberta.ca>
2018-12-22 20:46:09 -0800

    added CV route

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Redirect, Switch, Link, HashRouter } from "react-router-dom";
 import Landing from "./landing/landing";
-import HomeContainer from "./home/home_container";
+import CVContainer from "./cv/cv_container";
 import CompanyContainer from "./company/company_container";
 import ResearchContainer from "./research/research_container";
 
@@ -10,7 +10,7 @@ const App = () => (
   
       <Switch>
         <Route exact path="/" component={Landing} />
-        <Route exact path="/home" component={HomeContainer} />
+        <Route exact path="/cv" component={CVContainer} />
         <Route exact path="/company/:companyTik" component={CompanyContainer} />
         <Route exact path="/research/:companyTik" component={ResearchContainer} />
       </Switch>
