67568f5e4c5e49657f5d3649241f56ab375a0f6e
Nigel Rodrigues <nigel1@ualberta.ca>
2018-12-22 20:32:17 -0800

    optijos

--- src/components/landing/landing.jsx
+++ src/components/landing/landing.jsx
@@ -4,9 +4,15 @@ import { Link } from "react-router-dom";
 const landing = () => {
   return (
     <div className="landing">
-      <div>Hello,</div>
-      <div>Nigel Rodrigues</div>
-      <div>Options</div>
+      <div className="greeting">Hello,</div>
+      <div className="name">Nigel Rodrigues</div>
+      <div className="options">
+        <div>CV</div>
+        <div>Photography</div>
+        <div>Projects</div>
+        <div>About</div>
+        <div>Travel</div>
+      </div>
     </div>
   );
 };
