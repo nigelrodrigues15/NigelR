2f76760ebf379ae4de041d286f064dda2685e5ea
Nigel Rodrigues <nigel1@ualberta.ca>
2019-01-26 17:14:31 -0800

    p tag inside of a ul

--- src/components/cv/cv.jsx
+++ src/components/cv/cv.jsx
@@ -65,11 +65,11 @@ class CV extends React.Component {
                 <div className="cv-date">May 2018 - Aug 2018</div>
               </div>
               <ul>
-              <li>
-                Intensive! coding bootcamp focused on full stack web
+              <p>
+                Intensive coding bootcamp focused on full stack web
                 development using Ruby on Rails, Nodejs, and React
                 frameworks.{" "}
-              </li>
+              </p>
               </ul>
             </li>
             <li>
